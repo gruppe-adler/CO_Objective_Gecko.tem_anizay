e6328c1637fbc1c2f1678afb26fe21e2123d7b67
Eras308 <31324871+Eras308@users.noreply.github.com>
2019-06-18 21:39:43 +0200

    Eras First

---
+++ USER/missionInfo.hpp
@@ -0,0 +1,10 @@
+/*
+*   Legt allgemeine Information über die Mission fest.
+*/
+
+author = "Eras für Gruppe Adler";                                               // Missionsersteller
+onLoadName = "Operation Desert Extract";                                                   // Name der Mission
+onLoadMission = "Move, shoot, communicate.";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+loadScreen = "data\loadpic.paa";                                                // Ladebild
+overviewPicture = "data\loadpic.paa";                                                           // Bild, das in der Missionsauswahl angezeigt wird
+overviewText = "Move, shoot, communicate.";                                                              // Text, der in der Missionsauswahl angezeigt wird
