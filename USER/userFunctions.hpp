9450b2e02b570e1c570bbdc44aa9c08079c9aa4c
Eras308 <31324871+Eras308@users.noreply.github.com>
2019-07-11 20:49:45 +0200

    Update userFunctions.hpp

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -5,14 +5,9 @@
 
 //SUSHI SCRIPT ============================================================
 
-/*#define SAF_DEF
-    #include "..\sushi_scripts\core\init.cpp"
-    #include "..\sushi_scripts\breach\init.cpp"
-#undef SAF_DEF
-
 class SAF {
     #define SAF_FNCS
         #include "..\sushi_scripts\core\init.cpp"
         #include "..\sushi_scripts\breach\init.cpp"
     #undef SAF_FNCS
-};*/
+};
